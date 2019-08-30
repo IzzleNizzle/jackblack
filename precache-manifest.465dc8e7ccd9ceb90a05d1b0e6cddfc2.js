0e06293fb9fd1471882d3c23638903834dbdf20a
Iz P <pehisaac@gmail.com>
2019-08-29 20:50:04 -0600

    Updates

---
+++ precache-manifest.465dc8e7ccd9ceb90a05d1b0e6cddfc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110655288352c3bb0b7b83c4d6400d8d",
+    "url": "/jackblack/index.html"
+  },
+  {
+    "revision": "088437631f17eff69b73",
+    "url": "/jackblack/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "afb4918839768eac523b",
+    "url": "/jackblack/static/js/2.35d809c1.chunk.js"
+  },
+  {
+    "revision": "088437631f17eff69b73",
+    "url": "/jackblack/static/js/main.0fa1f9a3.chunk.js"
+  },
+  {
+    "revision": "b6109aff3e00ba4cd11f",
+    "url": "/jackblack/static/js/runtime~main.b3c33d63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jackblack/static/media/logo.5d5d9eef.svg"
+  }
+]);
